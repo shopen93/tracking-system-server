fb69d1f7c8cecacfb4984c9f03e74728f718bba6
Mateusz Witkowski <shopen93@gmail.com>
2017-02-22 19:23:05 +0100

    początkowa struktura serwera

---
+++ src/main/java/pl/lodz/TrackingSystemApplication.java
@@ -0,0 +1,12 @@
+package pl.lodz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/lodz/TrackingSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.lodz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrackingSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
